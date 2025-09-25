357215add7c5e990371ab638f332045e4040beaf
Suryakarthik-01 <karthikkumar9393@gmail.com>
2025-09-25 17:33:15 +0530

    changes made

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -14,13 +14,14 @@ import send_button from './send_button.svg'
 import menu_icon from './menu_icon.png'
 import arrow_icon from './arrow_icon.png'
 import code from './code.svg'
-import bgImage from './bgImage.svg'
 import pic1 from './pic1.png'
 import pic2 from './pic2.png'
 import pic3 from './pic3.png'
 import pic4 from './pic4.png'
 import img1 from './img1.jpg'
 import img2 from './img2.jpg'
+
+const bgImage = "/bgImage.svg";
 
 const assets = {
     avatar_icon,
